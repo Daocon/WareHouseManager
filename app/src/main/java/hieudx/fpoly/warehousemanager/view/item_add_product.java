e7cdbff23f90ed3ee089798896452ceec85a5072
Tran Quang Truong <quangtruong20022003@gmail.com>
2023-11-01 19:26:00 +0700

    truong

---
+++ app/src/main/java/hieudx/fpoly/warehousemanager/view/item_add_product.java
@@ -0,0 +1,16 @@
+package hieudx.fpoly.warehousemanager.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hieudx.fpoly.warehousemanager.R;
+
+public class item_add_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_add_product);
+    }
+}
