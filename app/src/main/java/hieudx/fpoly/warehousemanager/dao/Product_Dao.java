239abce80fe1366a25213adcb28ccd20da3ddfd9
Tran Quang Truong <quangtruong20022003@gmail.com>
2023-11-07 16:22:07 +0700

    giao dien product

--- app/src/main/java/hieudx/fpoly/warehousemanager/dao/Product_Dao.java
+++ app/src/main/java/hieudx/fpoly/warehousemanager/dao/Product_Dao.java
@@ -1,4 +1,28 @@
 package hieudx.fpoly.warehousemanager.dao;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+
+import hieudx.fpoly.warehousemanager.SQliteDB.DBHelper;
+import hieudx.fpoly.warehousemanager.models.Product;
+
 public class Product_Dao {
+    private DBHelper dbHelper;
+
+
+    public Product_Dao(Context context) {
+        this.dbHelper = new DBHelper(context);
+    }
+
+    public ArrayList<Product> getProductList(){
+        ArrayList<Product> list = new ArrayList<>();
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        Cursor cursor = database.rawQuery("Select * from Product",null);
+
+
+        return list;
+    }
 }
