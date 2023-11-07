4a90ca8c79f38d2a540e034d07fe5f56f40725b2
hieudxx <dxhieu03@gmail.com>
2023-11-07 07:24:51 +0700

    add bot_sheet_sort_bill

--- app/src/main/java/hieudx/fpoly/warehousemanager/fragments/Bill/Bill_In/Bill_In_Fragment.java
+++ app/src/main/java/hieudx/fpoly/warehousemanager/fragments/Bill/Bill_In/Bill_In_Fragment.java
@@ -1,20 +1,27 @@
 package hieudx.fpoly.warehousemanager.fragments.Bill.Bill_In;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import hieudx.fpoly.warehousemanager.R;
 import hieudx.fpoly.warehousemanager.adapters.Bill_In_Adapter;
 import hieudx.fpoly.warehousemanager.dao.Bill.Bill_In_Dao;
-import hieudx.fpoly.warehousemanager.databinding.FragmentBillBinding;
+import hieudx.fpoly.warehousemanager.databinding.BotSheetSortBillBinding;
 import hieudx.fpoly.warehousemanager.databinding.FragmentBillInBinding;
 import hieudx.fpoly.warehousemanager.models.Bill_In;
 
@@ -32,6 +39,52 @@ public class Bill_In_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentBillInBinding.inflate(inflater, container, false);
 
+        init();
+        onClickSort();
+
+        return binding.getRoot();
+    }
+
+    private void onClickSort() {
+        binding.imgSort.setOnClickListener(view -> {
+            Dialog dialog = new Dialog(getContext());
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+            BotSheetSortBillBinding btnBinding = BotSheetSortBillBinding.inflate(getLayoutInflater());
+            dialog.setContentView(btnBinding.getRoot());
+
+            btnBinding.rdGr.setOnCheckedChangeListener(((radioGroup, i) -> {
+                if (i == R.id.rd_sort_asc) {
+                    Collections.sort(list, new Comparator<Bill_In>() {
+                        @Override
+                        public int compare(Bill_In bill_in, Bill_In bill_in1) {
+                            return Integer.compare(bill_in.getTotal(), bill_in1.getTotal());
+                        }
+                    });
+                    adapter.notifyDataSetChanged();
+
+                } else {
+                    Collections.sort(list, new Comparator<Bill_In>() {
+                        @Override
+                        public int compare(Bill_In bill_in, Bill_In bill_in1) {
+                            return Integer.compare(bill_in1.getTotal(), bill_in.getTotal());
+                        }
+                    });
+                    adapter.notifyDataSetChanged();
+                }
+            }));
+
+            dialog.show();
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
+        });
+
+    }
+
+    private void init() {
         dao = new Bill_In_Dao(getContext());
         list = dao.getAll();
         if (!list.isEmpty()) {
@@ -39,7 +92,7 @@ public class Bill_In_Fragment extends Fragment {
             binding.imgSort.setVisibility(View.VISIBLE);
             binding.rcv.setVisibility(View.VISIBLE);
             binding.fltAdd.setVisibility(View.VISIBLE);
-            adapter = new Bill_In_Adapter(getContext(),list);
+            adapter = new Bill_In_Adapter(getContext(), list);
             binding.rcv.setAdapter(adapter);
         } else {
             binding.btnAdd.setVisibility(View.VISIBLE);
@@ -55,10 +108,6 @@ public class Bill_In_Fragment extends Fragment {
         binding.fltAdd.setOnClickListener(view -> {
             loadFragment();
         });
-
-
-
-        return binding.getRoot();
     }
 
     private void loadFragment() {
